8869e8334f8c5ffe1b1b5bdbda64e222f8252172
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2022-02-19 12:36:21 -0500

    copy only .py files, add note about circup

--- send.sh
+++ send.sh
@@ -5,8 +5,9 @@ if [ ! -d $DESTDIR ]; then
 	echo "Don't see ${DESTDIR}; you may need to change this script."
 	exit 1
 fi
-cp ./*py ${DESTDIR}
-cp lib/*py  ${DESTDIR}/lib
-# we don't copy third-party libraries here. I recommend you use a tool to install and update
-# those. Check out VSCode 'Circuitpython' extension, or Adafruit's CircUp
-# (https://github.com/adafruit/circup/)
+
+cp ./*.py ${DESTDIR}
+cp lib/*.py ${DESTDIR}
+
+# Circuitpython library modules are installed by circup (https://github.com/adafruit/circup/)
+# see files board-requirements.in and board-requirements.txt
